e3139fc49bbe29bb4798c9449af725e09af096fe
seilylook <seilylook@naver.com>
2021-01-06 21:50:18 +0900

    2 commit

--- js_file.js
+++ js_file.js
@@ -1,1 +1,9 @@
 console.log("Hello world");
+var healthObj = {
+    name: "달리기",
+    lastTime : "PM 10 : 30", 
+    showHealth() {
+        console.log(this.name + "님, 오늘은 " + this.lastTime + "에 운동을 하였습니다.")
+    }
+}
+healthObj.showHealth();
